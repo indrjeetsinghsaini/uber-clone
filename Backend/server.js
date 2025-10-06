9c73cbe85c8606d9c3e5693069507cb8ab7cadc0
unknown <premsinghsaini6395@gmai.com>
2025-10-06 12:35:26 -0700

    feat: add route logging for debug

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,7 @@
+
 const http = require('http');
-const app = require("./app");
+const app = require('./app');
+const listEndpoints = require('express-list-endpoints'); // ADD THIS LINE
 const { initializeSocket } = require('./socket');
 const port = process.env.PORT || 3000;
 
@@ -9,4 +11,7 @@ initializeSocket(server);
 
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
+    console.log("--- REGISTERED ROUTES ---");   // ADD THIS LINE
+    console.log(listEndpoints(app));            // ADD THIS LINE
+    console.log("-----------------------");   // ADD THIS LINE
 });
