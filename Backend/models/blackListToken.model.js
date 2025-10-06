b14439074bd0aa5c0d9b4f258bc73d8a986f1d4b
indrjeetsinghsaini <121189255+indrjeetsinghsaini@users.noreply.github.com>
2025-10-06 10:47:26 -0700

    Create blackListToken model for MongoDB

---
+++ Backend/models/blackListToken.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const blackListTokenSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+}, { timestamps: true });
+
+const BlackListToken = mongoose.model('BlackListToken', blackListTokenSchema);
+
+module.exports = BlackListToken;
